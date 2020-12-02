a2a050a4e44a565c801d74750fe7208267d31dd4
rosselton <rebecca.osselton@ncl.ac.uk>
2020-12-02 16:39:54 +0000

    Write to file functionality

--- process-rainfall.py
+++ process-rainfall.py
@@ -4,6 +4,7 @@ import hashlib
 import hmac
 import time
 import datetime
+import csv
 
 import urllib.parse
 import urllib.request
@@ -22,7 +23,7 @@ current_rain_total = 0
 rain_last10mins = 0
 rain_rate_last10mins = 0
 rain_rate_average = 0
-rain_duration = 0
+rain_duration_in_mins = 0
 
 
 # New hobo data download 
@@ -47,51 +48,56 @@ def _sum(arr):
       
 print('\nDateTime|Rain last 10 mins|Total rainfall|Rain rate|Rain rate last 10 mins|Rainfall duration\n')
 
-# loop through the data set using the total number of observations as a limit
-for i in range(len(response['observations'])):
+with open('rain_data_out.csv', mode='w',  newline='') as csv_file:
+    fieldnames = ['last recorded time', 'rain last 10 mins', 'current rain total', 'rain rate last 10 mins', 'rain rate average', 'rain duration in mins' ]
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    writer.writeheader()
 
-    # get each rain rate measurement
-    rain_rate_last15mins =response['observations'][i]['metric']['precipRate']
+    # loop through the data set using the total number of observations as a limit
+    for i in range(len(response['observations'])):
 
-    # check for invalid values, use previous value instead
-    if(rain_rate_last15mins > 300) or (rain_rate_last15mins < 0):
-        rain_rate_last15mins = response['observations'][i-1]['metric']['precipRate'] 
+        # get each rain rate measurement
+        rain_rate_last10mins =response['observations'][i]['metric']['precipRate']
+
+        # check for invalid values, use previous value instead
+        if(rain_rate_last10mins > 300) or (rain_rate_last10mins < 0):
+            rain_rate_last10mins = response['observations'][i-1]['metric']['precipRate'] 
+        
+        # add to array (for debug)
+        rain_rates.append(rain_rate_last10mins)
+        if(i != 0):
+            prev_rain_total=(response['observations'][i-1]['metric']['precipTotal'] )
+            print('prev_rain_total', prev_rain_total)
+            current_rain_total=(response['observations'][i]['metric']['precipTotal'] )
+            print('current_rain_total',  current_rain_total)
+            rain_last10mins = (current_rain_total-prev_rain_total)
+            rain_last10mins = round(rain_last10mins, 2)
+            print('rain_last15mins', rain_last10mins, '\n')
+
+        last_recorded_timeUTC=(response['observations'][i]['obsTimeUtc'] )
+        ## format last recorded time
+        last_recorded_time = datetime.datetime.strptime(last_recorded_timeUTC, "%Y-%m-%dT%H:%M:%SZ")
+        print('last_recorded_time', last_recorded_time)
+        # calculate the sum of all rain rate obs
+        sum_of_rain_rates = _sum(rain_rates)
+        sum_of_rain_rates = round(sum_of_rain_rates,2)
+        # avoid division by zero on the first row
+        if(i != 0):
+            rain_rate_average = sum_of_rain_rates / i
+            rain_rate_average = round(rain_rate_average, 2)
+            # add to array for debug
+            averages.append(rain_rate_average)
+            # increment for each 10 minute period
+            rain_duration_in_mins = rain_duration_in_mins + 10
+
+        print(last_recorded_time ,'|', rain_last10mins,'|', current_rain_total, '|', rain_rate_last10mins, '|', rain_rate_average, '|', rain_duration_in_mins, '\n') 
+        writer.writerow({'last recorded time': last_recorded_time, 'rain last 10 mins': rain_last10mins, 'current rain total': current_rain_total, 'rain rate last 10 mins': rain_rate_last10mins, 'rain rate average': rain_rate_average, 'rain duration in mins': rain_duration_in_mins })
     
-    # add to array (for debug)
-    rain_rates.append(rain_rate_last15mins)
-    if(i != 0):
-        prev_rain_total=(response['observations'][i-1]['metric']['precipTotal'] )
-        print('prev_rain_total', prev_rain_total)
-        current_rain_total=(response['observations'][i]['metric']['precipTotal'] )
-        print('current_rain_total',  current_rain_total)
-        rain_last15mins = (current_rain_total-prev_rain_total)
-        rain_last15mins = round(rain_last10mins, 2)
-        print('rain_last15mins', rain_last10mins, '\n')
-
-    last_recorded_timeUTC=(response['observations'][i]['obsTimeUtc'] )
-    ## format last recorded time
-    last_recorded_time = datetime.datetime.strptime(last_recorded_timeUTC, "%Y-%m-%dT%H:%M:%SZ")
-    print('last_recorded_time', last_recorded_time)
-    # calculate the sum of all rain rate obs
-    sum_of_rain_rates = _sum(rain_rates)
-    sum_of_rain_rates = round(sum_of_rain_rates,2)
-    # avoid division by zero on the first row
-    if(i != 0):
-        #print('row :', i, 'sum of rain rat :', sum_of_rain_rates, '\n')
-        rain_rate_average = sum_of_rain_rates / i
-        rain_rate_average = round(rain_rate_average, 2)
-        # add to array for debug
-        averages.append(rain_rate_average)
-        # increment for each 10 minute period
-        rain_duration = rain_duration + 10
-        print(last_recorded_time ,'|', rain_last10mins,'|', sum_of_rain_rates, '|', rain_rate_last10mins, '|', rain_rate_average, '|', rain_duration, '\n')
-    else:
-        print(last_recorded_time ,'|', rain_last10mins,'|', sum_of_rain_rates, '|', rain_rate_last10mins, '|', rain_rate_average, '|', rain_duration, '\n')    
-    i = i + 1
+        i = i + 1
     
    
-#print(rain_rates)
-#print(averages)
+print(rain_rates)
+print(averages)
 
 
 
