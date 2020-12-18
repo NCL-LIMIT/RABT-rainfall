2210e44dd81cbd3309e321b6b89960695096ab85
rosselton <rebecca.osselton@ncl.ac.uk>
2020-12-18 16:56:47 +0000

    Add sleep

--- process-rainfall.py
+++ process-rainfall.py
@@ -27,129 +27,124 @@ send_message = 1
 write_to_file = 0
 
 
-# New hobo data download 
-# uncomment this block to run with live api data
-#allDay="https://api.weather.com/v2/pws/observations/all/1day?stationId=IALEXA29&format=json&units=m&apiKey=4a83daf5d1b3462d83daf5d1b3f62d8f"
-#api_obs = requests.get(allDay)
-#response = api_obs.json()
+while(True):
+    time.sleep(600)
 
-# run with static file data
-with open('sample_rain_data.json') as f:
-    file_data = json.load(f)
+    # New hobo data download 
+    # uncomment this block to run with live api data
+    allDay="https://api.weather.com/v2/pws/observations/all/1day?stationId=IALEXA29&format=json&units=m&apiKey=4a83daf5d1b3462d83daf5d1b3f62d8f"
+    api_obs = requests.get(allDay)
+    response = api_obs.json()
 
-response = file_data
-#print(response) 
+    # run with static file data
+    #with open('sample_rain_data.json') as f:
+        #file_data = json.load(f)
 
-# iterate through the array and add each element to the sum variable one at a time 
-def _sum(arr):   
-    sum = 0 
-    for i in arr: 
-        sum = sum + i      
-    return(sum) 
+    #response = file_data
+    #print(response) 
+
+    # iterate through the array and add each element to the sum variable one at a time 
+    def _sum(arr):   
+        sum = 0 
+        for i in arr: 
+            sum = sum + i      
+        return(sum) 
 
       
-print('\nDateTime|Rain last 10 mins|Total rainfall|Rain rate|Rain rate last 10 mins|Rainfall duration\n')
+    print('\nDateTime|Rain last 10 mins|Total rainfall|Rain rate|Rain rate last 10 mins|Rainfall duration\n')
 
-# if write to file is required, open the file handle and write the header
-if(write_to_file ==1):
-    with open('rain_data_out.csv', mode='w',  newline='') as csv_file:
-        fieldnames = ['last recorded time', 'rain last 10 mins', 'current rain total', 'rain rate last 10 mins', 'rain rate average', 'rain duration in mins' ]
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        writer.writeheader()
+    # if write to file is required, open the file handle and write the header
+    if(write_to_file ==1):
+        with open('rain_data_out.csv', mode='w',  newline='') as csv_file:
+            fieldnames = ['last recorded time', 'rain last 10 mins', 'current rain total', 'rain rate last 10 mins', 'rain rate average', 'rain duration in mins' ]
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+            writer.writeheader()
 
+    # increment for each 10 minute period
+    rain_duration_in_mins = rain_duration_in_mins + 10
 
-# loop through the data set using the total number of observations as a limit
-for i in range(len(response['observations'])):
+    # loop through the data set using the total number of observations as a limit
+    for i in range(len(response['observations'])):
 
-    # get each rain rate measurement
-    rain_rate_last10mins =response['observations'][i]['metric']['precipRate']
+        # get each rain rate measurement
+        rain_rate_last10mins =response['observations'][i]['metric']['precipRate']
 
-    # check for invalid values: if it's not the first reading use previous value instead, otherwise reset to zero
-    if(rain_rate_last10mins > 300) or (rain_rate_last10mins < 0):
+        # check for invalid values: if it's not the first reading use previous value instead, otherwise reset to zero
+        if(rain_rate_last10mins > 300) or (rain_rate_last10mins < 0):
+            if(i != 0):
+                rain_rate_last10mins = response['observations'][i-1]['metric']['precipRate']
+            else:      
+                rain_rate_last10mins = 0.0
+
+        # store all the rain rates
+        rain_rates.append(rain_rate_last10mins)
+
+        # if its not the first reading use the previous value
         if(i != 0):
-            rain_rate_last10mins = response['observations'][i-1]['metric']['precipRate']
-        else:      
-            rain_rate_last10mins = 0.0
-
-    # store all the rain rates
-    rain_rates.append(rain_rate_last10mins)
-
-    # if its not the first reading use the previous value
-    if(i != 0):
-        prev_rain_total=(response['observations'][i-1]['metric']['precipTotal'] )
-       
-
-    print('prev_rain_total', prev_rain_total)
-    current_rain_total=(response['observations'][i]['metric']['precipTotal'] )
-    print('current_rain_total',  current_rain_total)
-    rain_last10mins = (current_rain_total-prev_rain_total)
-    rain_last10mins = round(rain_last10mins, 2)
-    print('rain_last15mins', rain_last10mins, '\n')
-
-    last_recorded_timeUTC=(response['observations'][i]['obsTimeUtc'] )
-    ## format last recorded time
-    last_recorded_time = datetime.datetime.strptime(last_recorded_timeUTC, "%Y-%m-%dT%H:%M:%SZ")
-    print('last_recorded_time', last_recorded_time)
-    # calculate the sum of all rain rate obs
-    sum_of_rain_rates = _sum(rain_rates)
-    sum_of_rain_rates = round(sum_of_rain_rates,2)
-
-    # avoid division by zero on the first row
-    if(i != 0):
-        rain_rate_average = sum_of_rain_rates / i
-        rain_rate_average = round(rain_rate_average, 2)
-        # increment for each 10 minute period
-        rain_duration_in_mins = rain_duration_in_mins + 10
-
-    print(last_recorded_time ,'|', rain_last10mins,'|', current_rain_total, '|', rain_rate_last10mins, '|', rain_rate_average, '|', rain_duration_in_mins, '\n') 
-    
-    # write each row to file
-    if (write_to_file == 1):
-        writer.writerow({'last recorded time': last_recorded_time, 'rain last 10 mins': rain_last10mins, 'current rain total': current_rain_total, 'rain rate last 10 mins': rain_rate_last10mins, 'rain rate average': rain_rate_average, 'rain duration in mins': rain_duration_in_mins })
-
-    # this will send a a message to rabbitmq given that it connects correctly
-    # if consumer.py is running at the same time, the messages will appear in the console, otherwise they will wait in the 'rainfall' queue
-   
-    # increment i
-    i = i + 1
-
-
-# only send last values
-if(send_message == 1):
-
-    # set up connection to  rabbitmq  
-    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-    if(connection):
-        channel = connection.channel() # start a channel
-
-        # not needed now as set up in rabbit config
-        # channel.exchange_declare(exchange='rabt-rainfall-exchange', exchange_type='direct')
-        # channel.queue_declare(queue='rabt-rainfall-queue') # declare a queue, then bind the queue to the exchange
-        # channel.queue_bind(queue='rabt-rainfall-queue', exchange='rabt-rainfall-exchange', routing_key='rabt-rainfall-queue')
-
-        # send a message
-        # routing key must match the queue name
-
-        json_map = {}
-        json_map["last recorded time"] = str(last_recorded_time)
-        json_map["rain last 10 mins"] = rain_last10mins
-        json_map["current rain total"] = current_rain_total
-        json_map["rain rate last 10 mins"] = rain_rate_last10mins
-        json_map["rain rate average"] = rain_rate_average
-        json_map["rain duration in  mins"] = rain_duration_in_mins
-        message = json.dumps(json_map)
+            prev_rain_total=(response['observations'][i-1]['metric']['precipTotal'] )
         
-        channel.basic_publish(exchange='rabt-rainfall-exchange', routing_key='rabt-rainfall-queue', body=message)
-        print ("Message sent to consumer")
-        connection.close()
-         
 
+        print('prev_rain_total', prev_rain_total)
+        current_rain_total=(response['observations'][i]['metric']['precipTotal'] )
+        print('current_rain_total',  current_rain_total)
+        rain_last10mins = (current_rain_total-prev_rain_total)
+        rain_last10mins = round(rain_last10mins, 2)
+        print('rain_last15mins', rain_last10mins, '\n')
 
+        last_recorded_timeUTC=(response['observations'][i]['obsTimeUtc'] )
+        ## format last recorded time
+        last_recorded_time = datetime.datetime.strptime(last_recorded_timeUTC, "%Y-%m-%dT%H:%M:%SZ")
+        print('last_recorded_time', last_recorded_time)
+        # calculate the sum of all rain rate obs
+        sum_of_rain_rates = _sum(rain_rates)
+        sum_of_rain_rates = round(sum_of_rain_rates,2)
 
+        # avoid division by zero on the first row
+        if(i != 0):
+            rain_rate_average = sum_of_rain_rates / i
+            rain_rate_average = round(rain_rate_average, 2)
 
+        print(last_recorded_time ,'|', rain_last10mins,'|', current_rain_total, '|', rain_rate_last10mins, '|', rain_rate_average, '|', rain_duration_in_mins, '\n') 
+        
+        # write each row to file
+        if (write_to_file == 1):
+            writer.writerow({'last recorded time': last_recorded_time, 'rain last 10 mins': rain_last10mins, 'current rain total': current_rain_total, 'rain rate last 10 mins': rain_rate_last10mins, 'rain rate average': rain_rate_average, 'rain duration in mins': rain_duration_in_mins })
 
+        # this will send a a message to rabbitmq given that it connects correctly
+        # if consumer.py is running at the same time, the messages will appear in the console, otherwise they will wait in the 'rainfall' queue
     
+        # increment i
+        i = i + 1
 
+    # only send last values
+    if(send_message == 1):
+
+        # set up connection to  rabbitmq  
+        connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+        if(connection):
+            channel = connection.channel() # start a channel
+
+            # not needed now as set up in rabbit config
+            # channel.exchange_declare(exchange='rabt-rainfall-exchange', exchange_type='direct')
+            # channel.queue_declare(queue='rabt-rainfall-queue') # declare a queue, then bind the queue to the exchange
+            # channel.queue_bind(queue='rabt-rainfall-queue', exchange='rabt-rainfall-exchange', routing_key='rabt-rainfall-queue')
+
+            # send a message
+            # routing key must match the queue name
+
+            json_map = {}
+            json_map["last recorded time"] = str(last_recorded_time)
+            json_map["rain last 10 mins"] = rain_last10mins
+            json_map["current rain total"] = current_rain_total
+            json_map["rain rate last 10 mins"] = rain_rate_last10mins
+            json_map["rain rate average"] = rain_rate_average
+            json_map["rain duration in  mins"] = rain_duration_in_mins
+            message = json.dumps(json_map)
+            
+            channel.basic_publish(exchange='rabt-rainfall-exchange', routing_key='rabt-rainfall-queue', body=message)
+            print ("Message sent to consumer")
+            connection.close()
+        
 
 
 
