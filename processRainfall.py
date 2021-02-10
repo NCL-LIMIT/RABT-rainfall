904fa1c97c7bdefd5caf228ccfbe827ec71149c3
rosselton <rebecca.osselton@ncl.ac.uk>
2021-02-10 16:01:55 +0000

    Set rain-rate-average to precipRate

--- processRainfall.py
+++ processRainfall.py
@@ -48,8 +48,7 @@ def handleResponse(res, send_message):
 
         # set up connection to  rabbitmq
         connection = create(rabbit_connection_string, connectionAttemptInterval)
-        # rabbit config sets up: exchange='rabt-rainfall-exchange', queue='rabt-rainfall-queue'
-
+       
         # send a message : routing key must match the queue name
         if send_message == 1:
             publish(connection, message, 'rabt-rainfall-queue', 'rabt-rainfall-exchange', 'direct')
@@ -73,41 +72,28 @@ def handleResponse(res, send_message):
     return confirmation_message
 
 def createRainfallMessage(response):
-    rain_rates = []
+
     prev_rain_total = 0
     current_rain_total = 0
     rain_last10mins = 0
-    rain_rate_last10mins = 0
     rain_rate_average = 0.0
     rain_duration_in_mins = 0
 
-
-    # iterate through the array and add each element to the sum variable one at a time
-    def _sum(arr):
-        sum = 0
-        for i in arr:
-            sum = sum + i
-        return (sum)
-
-        # increment for each 10 minute period
-
+    # increment for each 10 minute period
     rain_duration_in_mins = rain_duration_in_mins + 10
 
     # loop through the data set using the total number of observations as a limit
     for i in range(len(response['observations'])):
 
         # get each rain rate measurement
-        rain_rate_last10mins = response['observations'][i]['metric']['precipRate']
+        rain_rate_average = response['observations'][i]['metric']['precipRate']
 
         # check for invalid values: if it's not the first reading use previous value instead, otherwise reset to zero
-        if (rain_rate_last10mins > 300) or (rain_rate_last10mins < 0):
+        if (rain_rate_average > 300) or (rain_rate_average < 0):
             if (i != 0):
-                rain_rate_last10mins = response['observations'][i - 1]['metric']['precipRate']
+                rain_rate_average = response['observations'][i - 1]['metric']['precipRate']
             else:
-                rain_rate_last10mins = 0.0
-
-        # store all the rain rates
-        rain_rates.append(rain_rate_last10mins)
+                rain_rate_average = 0.0
 
         # if its not the first reading use the previous value
         if (i != 0):
@@ -120,16 +106,7 @@ def createRainfallMessage(response):
         last_recorded_timeUTC = (response['observations'][i]['obsTimeUtc'])
         ## format last recorded time
         last_recorded_time = datetime.datetime.strptime(last_recorded_timeUTC, "%Y-%m-%dT%H:%M:%SZ")
-        # calculate the sum of all rain rate obs
-        sum_of_rain_rates = _sum(rain_rates)
-        sum_of_rain_rates = round(sum_of_rain_rates, 2)
-
-        # avoid division by zero on the first row
-        if (i != 0):
-            # print('divide ', sum_of_rain_rates, 'by', i)
-            rain_rate_average = sum_of_rain_rates / i
-            rain_rate_average = round(rain_rate_average, 2)
-
+       
         # increment i
         i = i + 1
 
@@ -137,11 +114,10 @@ def createRainfallMessage(response):
     json_map["last-recorded-time"] = str(last_recorded_time)
     json_map["rain-last-10-mins"] = rain_last10mins
     json_map["current-rain-total"] = current_rain_total
-    json_map["rain-rate-last-10-mins"] = rain_rate_last10mins
     json_map["rain-rate-average"] = rain_rate_average
     json_map["rain-duration-in-mins"] = rain_duration_in_mins
     message = json.dumps(json_map)
-    print(last_recorded_time, '|', rain_last10mins, '|', current_rain_total, '|', rain_rate_last10mins, '|',
+    print(last_recorded_time, '|', rain_last10mins, '|', current_rain_total,  '|',
           rain_rate_average, '|', rain_duration_in_mins, '\n')
     return message
 
